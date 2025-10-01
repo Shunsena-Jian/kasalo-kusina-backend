7e58376a20068b9a08816fc894ec61ef520af3ab
Jian Raphael R Cudiamat <jian.r.cudiamat@gmail.com>
2025-10-01 17:46:21 +0800

    [Update 2] - Install redis session, prepped backend routes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+const userRoutes = require('./routes/users');
+const recipeRoutes = require('./routes/recipes');
+const authRoutes = require('./routes/auth');
+
+app.use('/api/users', userRoutes);
+app.use('/api/recipes', recipeRoutes);
+app.use('/api/auth',  authRoutes);
 
 app.get('/',  (req, res) => {
     res.send('Hello World');
---
+++ src/routes/users.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+// Get User Details
+router.get('/', (req, res) => {
+
+});
