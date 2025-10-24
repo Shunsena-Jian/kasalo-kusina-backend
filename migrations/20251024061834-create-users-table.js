2de4b085a0f64859eff694919ef5e7f12c9f8a30
Jian Raphael R Cudiamat <jian.r.cudiamat@gmail.com>
2025-10-24 14:33:11 +0800

    [2025-10-24 update_2] Add `db-migrate` configuration, user table migration, and update dependencies.

---
+++ migrations/20251024061834-create-users-table.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+export function setup(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+}
+
+export function up(db) {
+  return db.createTable('users', {
+      id: { type: 'int', length: 255, primaryKey: true },
+      username: { type: 'string', length: 255, notNull: true, unique: true },
+      password: { type: 'string', notNull: true },
+      email: { type: 'string', notNull: true, unique: true },
+      created_at: { type: 'timestamp', notNull: true, defaultValue: String('CURRENT_TIMESTAMP') },
+      updated_at: { type: 'timestamp', notNull: true, defaultValue: String('CURRENT_TIMESTAMP') }
+  }, { ifNotExists: true });
+}
+
+export function down(db) {
+  return null;
+}
+
+export const _meta = {
+  "version": 1
+};
